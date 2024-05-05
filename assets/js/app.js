b2a863bc98a7328524a7b2c2a9e01c5475cbb239
Corey Jordan <113132164+CoreyJordan@users.noreply.github.com>
2024-05-05 12:45:44 -0400

    Add Responsive burger menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,12 @@ const nav = document.querySelector("nav");
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
 });
+
+// Close hamburger menu when a link is closed
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
